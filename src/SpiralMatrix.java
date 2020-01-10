046389fc33e705b91e0c193300885f9f1929532f
Sanjana Shah <sanjanashah123@berkeley.edu>
2020-01-10 14:03:26 -0800

    Cleaned SpiralMatrix.java

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -13,41 +13,6 @@
  */
 import java.lang.*;
 public class SpiralMatrix {
-//    static int[][] spiralFill(int m, int n) {
-//        int[][] a = new int[n][n];
-//        int val = 1;
-//        int k = 0, l = 0;
-//        while (k < m && l < n) {
-//            for (int i = l; i < n; ++i) {
-//                a[k][i] = val++;
-//            }
-//            k++;
-//            for (int i = k; i < m; ++i) {
-//                a[i][n - 1] = val++;
-//            }
-//            n--;
-//            if (k < m) {
-//                for (int i = n - 1; i >= l; --i) {
-//                    a[m - 1][i] = val++;
-//                }
-//                m--;
-//            }
-//            if (l < n) {
-//                for (int i = m - 1; i >= k; --i) {
-//                    a[i][l] = val++;
-//                }
-//                l++;
-//            }
-//        }
-//        return a;
-//    }
-
-//    i used two variables, directionX and directionY. Changed it to +1 or -1 depending on which direction i want to go
-//    then add that to x and y
-//    matrix[x+directonX][y+directionY= num++
-//            if () directionX = -directionX
-//if () directionY = -directionY
-
     static int[][] spiralFill(int n) {
         int[][] arr = new int[n][n];
         int value = 1;
